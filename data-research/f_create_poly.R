3761f9ede2ff89865f1c28effd89237823f355e2
Malte Kyhos <mkyhos@protonmail.com>
2020-12-28 00:46:46 +0100

    Create poly file for osmosis filtering

---
+++ data-research/f_create_poly.R
@@ -0,0 +1,35 @@
+library(magrittr)
+#' Writes a simple polygon in polygon file format for osmosis
+#' 
+#' Assumes that there is only one polygon, no holes, etc.
+#' 
+#' @param sf_polygon Input polygon
+#' @param output_file <string> Path to desired output file. Wiht .poly ending.
+write_poly_file <- function(sf_polygon, output_file) {
+
+  polygon_part <- sf_polygon %>%
+    sf::st_transform(crs = 3857) %>%
+    sf::st_cast(to = "POINT") %>%
+    sf::st_coordinates() %>%
+    as.data.frame() %>%
+    setNames(c("lon", "lat")) %>%
+    dplyr::mutate_all(formatC, format = "e") %>%
+    dplyr::mutate(pasted = paste0("    ", lon, "    ", lat)) %>%
+    dplyr::pull(pasted) %>%
+    paste(collapse = "\n")
+
+  result <- glue::glue(
+    "
+    polygon_v
+    first_area
+    {polygon_part}
+    END
+    END
+
+    ")
+  writeLines(
+    text = result,
+    con = output_file
+    )
+
+}
