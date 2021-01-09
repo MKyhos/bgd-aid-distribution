3a01db31452cdb3bed7b63b05520d5982ed0d9b1
Malte Kyhos <mkyhos@protonmail.com>
2021-01-09 02:40:42 +0100

    Update missing env variables for backup script

--- data-backup/db-backup.sh
+++ data-backup/db-backup.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-# Using Authentification via a .pgpass file.
-#export PGPASSWORD=$(cat ~/.pgpass)
-
-DFILE="data-backup/gis-db-backup_`date +%Y-%m%-d_%H-%M-%S`.sql.gz"
-
-pg_dump \
-  --dbname=gis_db \
-  --username=gis_user \
-  --port=45432 \
-  --host=localhost |
-  gzip > $DFILE
+DFILE="data-backup/gis-db-backup_`date +%Y-%m-%d_%H-%M-%S`.sql.gz"
+pg_dump ${PG_URI} | gzip > $DFILE
 
 echo "SQL Backup completed."
+
