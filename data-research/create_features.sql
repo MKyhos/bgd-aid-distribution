aa1bdc21c007ab961c542bf22e1c9a4452c14ee2
Malte Kyhos <mkyhos@protonmail.com>
2021-01-23 15:11:13 +0100

    Rename osm_cxb_buildings to buildings

--- data-research/create_features.sql
+++ data-research/create_features.sql
@@ -2,27 +2,31 @@
 
 -- Create Table of OSM Buildings:
 
-CREATE TABLE osm_cxb_buildings AS (
+CREATE TABLE buildings AS (
   SELECT osm_id AS id,
     building AS building_type,
     g.camp_id AS camp_id,
     g.block_id AS block_id,
     g.sblock_id AS sblock_id,
     ST_Area(way) AS area_sqm,
-    ST_Centroid(way) AS geom
+    ST_Centroid(o.way) AS geom
   FROM osm_polygon AS o
   JOIN geo_admin AS g ON ST_Intersects(o.way, g.geom)
-  WHERE building IS NOT NULL
+  WHERE building IN ('yes', 'hut', 'residential', 'house', 'apartments')
 );
 
 CREATE INDEX osm_cxb_buildings_idx
   ON osm_cxb_buildings USING GIST(geom);
 
 
+CREATE INDEX buildings_idx
+  ON buildings USING GIST(geom);
+
+
 -- Assign population to Building, based on block ID and 
 
 -- Create additional column, set to 0 by default.
-ALTER TABLE osm_cxb_buildings
+ALTER TABLE buildings
   ADD COLUMN IF NOT EXISTS count_flooded int DEFAULT 0,
   ADD COLUMN IF NOT EXISTS population numeric DEFAULT 0,
   ADD COLUMN IF NOT EXISTS dist_tube numeric, -- Distance to tubewells
@@ -39,30 +43,27 @@ ALTER TABLE osm_cxb_buildings
 WITH
   building_flooded AS (
     SELECT id AS building_id, sblock_id, flood AS flood_id
-    FROM osm_cxb_buildings AS b
+    FROM buildings AS b
     INNER JOIN geo_floods AS f ON ST_Within(b.geom, f.geom)),
   building_count AS (
     SELECT building_id, Count(*) AS count 
     FROM building_flooded
     GROUP BY 1
   )
-UPDATE osm_cxb_buildings
+UPDATE buildings
   SET count_flooded = Coalesce(b.count, 0)
   FROM building_count AS b
-  WHERE osm_cxb_buildings.id = b.building_id;
+  WHERE buildings.id = b.building_id;
 
 -- Distribute Population to buil  dings according to the share of area of
 -- a building to the total building area in the respective block.
 -- Status: :checkmark:
 
 WITH 
-  block_building_areas AS (
-    SELECT block_id, Sum(area_sqm) AS camp_area_sum
-    FROM osm_cxb_buildings
+    FROM buildings
     GROUP BY 1
   )
-UPDATE osm_cxb_buildings AS b1
-  SET population = (d.pop_total * (b1.area_sqm / bba.camp_area_sum))
+UPDATE buildings AS b1
   FROM dta_block AS d
   JOIN block_building_areas AS bba ON d.block_id = bba.block_id
   WHERE b1.block_id = d.block_id;
@@ -70,7 +71,7 @@ UPDATE osm_cxb_buildings AS b1
 -- Get Nearest Neighbor distance for each house to various Elements...
 
 -- 1. Bathing stuff
-UPDATE osm_cxb_buildings AS b1
+UPDATE buildings AS b1
   SET dist_bath = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
@@ -80,7 +81,7 @@ UPDATE osm_cxb_buildings AS b1
   );
 
 -- 2. Tubewells
-UPDATE osm_cxb_buildings AS b1
+UPDATE buildings AS b1
   SET dist_tube = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
@@ -90,7 +91,7 @@ UPDATE osm_cxb_buildings AS b1
   );
 
 -- 3. nutrition servicves
-UPDATE osm_cxb_buildings AS b1
+UPDATE buildings AS b1
   SET dist_nutr = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
@@ -100,7 +101,7 @@ UPDATE osm_cxb_buildings AS b1
   );
 
  -- 4. Women protection zones
- UPDATE osm_cxb_buildings AS b1
+ UPDATE buildings AS b1
   SET dist_wpro = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
@@ -110,7 +111,7 @@ UPDATE osm_cxb_buildings AS b1
   );
 
 -- 5. Latrines 
-UPDATE osm_cxb_buildings AS b1
+UPDATE buildings AS b1
   SET dist_latr = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
@@ -120,7 +121,7 @@ UPDATE osm_cxb_buildings AS b1
   );
 
 -- 6. Health care facilities
-UPDATE osm_cxb_buildings AS b1
+UPDATE buildings AS b1
   SET dist_heal = (
     SELECT ST_Distance(b1.geom, gri.geom)
     FROM geo_reach_infra AS gri
